a3883c369eee54cf2a20dcf4c10f0a29dc21f05e
Trung Nguyen <ngthanhtrung23@gmail.com>
2020-01-13 00:22:39 +0800

    Solve 1285{A, B}.

---
+++ lib/vec.rs
@@ -0,0 +1,14 @@
+fn main() {
+	let mut scanner = Scanner::default();
+
+    // Read a Vec with n elements.
+	let n = scanner.next::<usize>();
+	let a: Vec<i64> = (0..n).map(|_| scanner.next()).collect();
+
+    // Calculate prefix sum of a vector: prefix[i] = sum(a[0]..a[i])
+	let prefix_sum = a.iter().scan(0, |sum, &x| {
+		*sum += x;
+		Some(*sum)
+	}).collect::<Vec<_>>();
+}
+
