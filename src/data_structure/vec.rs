57972f8223d7f08bf7400a95c9b17e3dcc4d1584
Trung Nguyen <ngthanhtrung23@gmail.com>
2020-01-14 00:34:39 +0800

    Rewrite vec.rs and add some tests.

--- src/data_structure/vec.rs
+++ src/data_structure/vec.rs
@@ -1,16 +1,26 @@
 use super::super::scanner::Scanner;
 
-fn main() {
-	let mut scanner = Scanner::default();
+/// Read n space-separated i64 into a Vec.
+fn read_vec<T: std::str::FromStr>(scanner: &mut Scanner, n: usize) -> Vec<T> {
+    (0..n).map(|_| scanner.next::<T>()).collect()
+}
 
-    // Read a Vec with n elements.
-	let n = scanner.next::<usize>();
-	let a: Vec<i64> = (0..n).map(|_| scanner.next()).collect();
-
-    // Calculate prefix sum of a vector: prefix[i] = sum(a[0]..a[i])
-	let prefix_sum = a.iter().scan(0, |sum, &x| {
+/// Returns prefix sum of a Vec.
+fn prefix_sum(a: &Vec<i64>) -> Vec<i64> {
+	return a.iter().scan(0, |sum, &x| {
 		*sum += x;
 		Some(*sum)
 	}).collect::<Vec<_>>();
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+	#[test]
+	fn test_prefix_sum() {
+		assert_eq!(prefix_sum(&vec![]), vec![]);
+		assert_eq!(prefix_sum(&vec![10, 20, 30]), vec![10, 30, 60]);
+		assert_eq!(prefix_sum(&vec![10, -20, 20]), vec![10, -10, 10]);
+	}
+}
